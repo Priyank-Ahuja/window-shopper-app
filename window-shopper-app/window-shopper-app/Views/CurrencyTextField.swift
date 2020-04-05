c8ce06527182fc72c44207813e047eea53aea252
Priyank Ahuja <priyankahuja02@gmail.com>
2020-04-05 10:28:47 +0530

    Input accessory view and IBDesignable

--- window-shopper-app/window-shopper-app/Views/CurrencyTextField.swift
+++ window-shopper-app/window-shopper-app/Views/CurrencyTextField.swift
@@ -8,15 +8,26 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTextField: UITextField {
 
     
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView() {
         backgroundColor = #colorLiteral(red: 0.999904573, green: 1, blue: 0.9998808503, alpha: 0.2531360035)
         layer.cornerRadius = 10
         layer.masksToBounds = true
         textAlignment = .center
+        keyboardType = .decimalPad
+        enablesReturnKeyAutomatically = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
@@ -24,12 +35,4 @@ class CurrencyTextField: UITextField {
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
     }
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
